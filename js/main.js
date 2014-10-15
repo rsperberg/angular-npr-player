503296f9f374517409a772e2d2b8fdc689fe00bc
rsperberg <rsperberg@gmail.com>
2014-10-15 19:31:55 -0400

    add npr api key, make  request

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 var app = angular.module('myApp', []);
+var apiKey = 'MDE3MDc1OTY3MDE0MTM0MTUzMzI4NjVmYw001'
+var nprUrl = 'http://api.npr.org/query?id=61&fields=relatedLink,title,byline,text,audio,image,pullQuote,all&output=JSON';
 
-app.controller('PlayerController', ['$scope', function($scope) {
+app.controller('PlayerController', ['$scope', function($scope, $http) {
     $scope.playing = false;
     $scope.audio = document.createElement('audio');
     $scope.audio.src = '/media/npr.mp4';
@@ -19,6 +21,27 @@ app.controller('PlayerController', ['$scope', function($scope) {
         $scope.$apply(function() {
             $scope.stop()
         });
+
+    $http({
+        method: 'JSONP',
+        url: nprUrl + '&apiKey=' + apiKey + '&callback=JSON_CALLBACK'
+      }).success(function(data, status) {
+        // Now we have a list of the stories (data.list.story)
+        // in the data object that the NPR API
+        // returns in JSON that looks like:
+        // data: { "list": {
+        //   "title": ...
+        //   "story": [
+        //     { "id": ...
+        //       "title": ...
+        // Store the list of stories on the scope
+        // from the NPR API response object (described above)
+        $scope.programs = data.list.story;
+
+      }).error(function(data, status) {
+        // Some error occurred
+      });
+
     });
 
 }]);  // PlayerController
